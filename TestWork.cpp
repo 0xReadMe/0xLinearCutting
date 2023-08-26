9f8f988ab9ad1baa9d72042000f13ff9f2617693
ReadME <mezerasana@gmail.com>
2023-08-26 23:11:01 +0300

    EnterFrame function update, add vector<Detail> and re-write all program for future scaling.

--- TestWork.cpp
+++ TestWork.cpp
@@ -3,40 +3,74 @@
 #include "CuttingAlgorithm.h";
 #include <iostream>
 #include <vector>
+#include <fstream>
 
-void EnterFrame(int& bLength, int& d1Length, int& d1Count, int& d2Length, int& d2Count, Baseboard& baseboard, Detail& dTypeOne, Detail& dTypeTwo)
+void EnterFrame(int& detailCount, Baseboard& baseboard, std::fstream& fout)
 {
-	std::cout << "Enter a count of details for cutting: ";
-	std::cout << "Enter a baseboard length:"; std::cin >> bLength;
-	std::cout << "Enter a detail length:"; std::cin >> d1Length;
-	std::cout << "Enter a detail count:"; std::cin >> d1Count; 
-	std::cout << ":"; std::cin >> d1Count;
-	std::cout << std::endl;
+	int					choice			{};
+	int					bLength			{};
+	std::string			choice_str		{};
+	std::string			foutWriteString	{};
 
+	std::cout << "Select the data input method: \n 1 - console \n 2 - file:"; std::cin >> choice;
+	switch (choice)
+	{
+		case 1:
+			std::cout << "Enter a count of details for cutting: "; std::cin >> detailCount;
+			std::cout << "Enter a baseboard length:"; std::cin >> bLength;
+
+			break;
+
+		case 2:
+			std::cout << "Enter a count of details for cutting: "; std::cin >> detailCount;
+			std::cout << "Enter a baseboard length:"; std::cin >> bLength;
+			foutWriteString = "Baseboard length >> " + bLength; foutWriteString += ";";
+			fout << foutWriteString + "\n";
+
+			for (int i = 0; i <= detailCount; i++) 
+			{
+				foutWriteString = "Detail " + i; foutWriteString += " count: 0;";
+				fout << foutWriteString + "\n";
+			}
+			for (int i = 0; i <= detailCount; i++)
+			{
+				foutWriteString = "Detail " + i; foutWriteString += " length: 0;";
+				fout << foutWriteString + "\n";
+			}
+
+			std::cout << "We create a file in main .exe directory.\nPlease, open it and write data for each detail, and then go back to this windows and enter +: \n>>"; std::cin >> choice_str;
+			if (choice_str == "+") 
+			{
+			
+			}
+			else 
+			{
+				std::cout << "You enter incorrect data in console window. \nPlease, start again.";
+			}
+			break;
+
+		default:
+			break;
+	}
+	
+
+	std::cout << std::endl;
 	baseboard.SetLength(bLength);
-	dTypeOne.SetLength(d1Length);
-	dTypeOne.SetCount(d1Count);
-	dTypeTwo.SetLength(d2Length);
-	dTypeTwo.SetCount(d2Count);
+	
 }
 
 int main()
 {
-	Detail				detailTypeOne	{};		// деталь первого типа
-	Detail				detailTypeTwo	{};		// деталь второго типа
+	std::fstream fout("data.txt", std::ios::out | std::ios::trunc);
+
 	Baseboard			baseboard		{};		// плинтус
-	int					refuse			{};		// отход
 	CuttingAlgorithm	cut				{};		// инициализация класса алгоритмов раскроя
+	int					refuse			{};		// отход
 	int					detailCount		{};
 	std::vector<Detail> details			{};
 
-	int bLength	{};								//__
-	int d1Length{};								//  |
-	int d2Length{};								//   > переменные для ввода с консоли и установки значений через setter
- 	int d1Count	{};								//  |
-	int d2Count	{};								//--
 	
-	EnterFrame(bLength, d1Length, d2Length, d1Count, d2Count, baseboard, detailTypeOne, detailTypeTwo);
+	EnterFrame(detailCount, baseboard, fout);
 	
 	while (1)
 	{
@@ -48,14 +82,14 @@ int main()
 		switch (choice)
 		{
 			case 1:
-				baseboard.SetCount(cut.SimpleCutting(detailTypeOne, detailTypeTwo, baseboard, refuse));
+				//baseboard.SetCount(cut.SimpleCutting(detailTypeOne, detailTypeTwo, baseboard, refuse));
 				std::cout << "\nYou need to buy a " << baseboard.GetCount() << " baseboards. \n";
 				std::cout << "Your refuse: " << refuse << "\n\n";
 				break;
 
 			case 2:
-				cut.GreedyCutting(detailTypeOne, baseboard, refuse);
-				cut.GreedyCutting(detailTypeTwo, baseboard, refuse);
+				//cut.GreedyCutting(detailTypeOne, baseboard, refuse);
+				//cut.GreedyCutting(detailTypeTwo, baseboard, refuse);
 				std::cout << "\nYou need to buy a " << baseboard.GetCount() << " baseboards. \n";
 				std::cout << "Your refuse: " << refuse << "\n\n";
 
@@ -64,7 +98,7 @@ int main()
 				break;
 
 			case 4:
-				EnterFrame(bLength, d1Length, d2Length, d1Count, d2Count, baseboard, detailTypeOne, detailTypeTwo);
+				EnterFrame(detailCount, baseboard, fout);
 				break;
 
 			default:
